9692bf752aae981dcf66f1cb4d520046dadc5823
KJC <kjc@KJCs-MacBook-Air.local>
2024-03-12 15:28:56 +0800

    Push

--- color_app/views.py
+++ color_app/views.py
@@ -45,7 +45,7 @@ def random_color_view(request):
 class ColorListView(ListView):                                                                                         
     model = Color                                                                                                      
     template_name = "color_app/color_list.html"                                                                       
-    queryset = Color.objects.order_by("name")                                                                          
+    queryset = Color.objects.order_by("-red")                                                                          
                                                                                                                        
 class NewColorView(CreateView):                                                                                        
     model = Color                                                                                                      
